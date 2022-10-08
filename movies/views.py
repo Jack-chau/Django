655b5dc97d739b751f82c4ed4c901f9fa8aec9b9
Jack-chau <chaukaho149@gmail.com>
2022-10-08 12:29:08 +0800

    modify movies.html and view.py for learning template setup ~

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# we can save data as globle variable
+data = {
+    "movies" : [
+        {
+            'id' : 5,
+            'title' : 'Jaws',
+            'years' : 1669,
+        },
+        {
+            'id' : 6,
+            'title' : 'Sharknado',
+            'years' : 1600,
+        },
+        {
+            'id' : 7,
+            'title' : 'The Mag',
+            'years' : 2000,
+        },
+    ]
+}
+
 def home ( request ) :
     return HttpResponse ( "Home page" )
 
 def movies  ( request ) :
-    # render ( argument, 'template path', <data> )
-    return render ( request, 'movies/movies.html', { 'movies': [ 'movies1', 'movies2' ] } )
+    # template render take three arrguments ( argument, 'template path', <data> )
+    return render ( request, 'movies/movies.html', data )
 
